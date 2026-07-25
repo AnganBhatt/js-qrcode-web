c1e8cbf4eb4dd7f5a48fb03edcbf8d25c0d0365c
AnganBhatt <anganbh99@gmail.com>
2026-07-25 21:00:19 +0600

    Add files via upload

---
+++ asset/script.js
@@ -0,0 +1,56 @@
+// mobile-menu js
+const menuopen = document.querySelector('.hamburger');
+const menuclose = document.querySelector('.collaps-btn');
+const mobilemenu = document.querySelector('.menu');
+const navhide = document.querySelector('nav');
+
+menuopen.addEventListener("click", function () {
+    mobilemenu.classList.add('active');
+    navhide.classList.add('hide-nav');
+});
+
+menuclose.addEventListener('click', function () {
+    mobilemenu.classList.remove('active');
+    navhide.classList.remove('hide-nav');
+
+});
+
+
+// large-scree js
+const bgshow = document.querySelectorAll('.nav-links');
+
+const home = bgshow[0];
+const product = bgshow[1];
+const contact = bgshow[2];
+const about = bgshow[3];
+
+home.addEventListener('click', function () {
+    home.classList.add('active');
+    product.classList.remove('active');
+    about.classList.remove('active');
+    contact.classList.remove('active');
+});
+
+product.addEventListener('click', function () {
+    product.classList.add('active');
+    about.classList.remove('active');
+    contact.classList.remove('active');
+    home.classList.remove('active');
+
+});
+
+contact.addEventListener('click', function () {
+    product.classList.remove('active');
+    about.classList.remove('active');
+    contact.classList.add('active');
+    home.classList.remove('active');
+
+});
+
+about.addEventListener('click', function () {
+    product.classList.remove('active');
+    about.classList.add('active');
+    contact.classList.remove('active');
+    home.classList.remove('active');
+
+});
